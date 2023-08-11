fb3f62659a772c126752183a0bf1a734c5a6fea4
Alain Guillon <zyrass@outlook.fr>
2023-08-11 11:29:59 +0200

    feature: Create User Model
    Create a UserSchema

---
+++ models/User.model.js
@@ -0,0 +1,70 @@
+/**
+ * #### Mongoose
+ * @version 7.0.3
+ * @see https://www.npmjs.com/package/mongoose
+ */
+const mongoose = require('mongoose');
+/**
+ * #### Bcrypt
+ * @version 5.1.0
+ * @see https://www.npmjs.com/package/bcrypt
+ */
+const bcrypt = require('bcrypt');
+
+/**
+ * _COLLECTION_NAME fait référence à MONGODB_COLLECTION_NAME issu du fichier .env_
+ */
+const COLLECTION_NAME = process.env.MONGODB_COLLECTION_NAME;
+
+/**
+ * _Définition du schéma pour un utilisateur_
+ */
+const UserSchema = mongoose.Schema(
+    {
+        firstname: {
+            type: mongoose.Schema.Types.String,
+            required: true,
+        },
+        lastname: {
+            type: mongoose.Schema.Types.String,
+            required: true,
+        },
+        email: {
+            type: mongoose.Schema.Types.String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: mongoose.Schema.Types.String,
+            required: true,
+        },
+        passwordConfirmation: {
+            type: mongoose.Schema.Types.String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+/**
+ * Middleware .pre qui permet de controller le password et de l'encrypter
+ */
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        if (this.password !== this.passwordConfirmation) {
+            return next(new Error('Les mots de passe ne correspondent pas.'));
+        } else {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                this.password = await bcrypt.hash(this.password, salt);
+            } catch (error) {
+                return next(error);
+            }
+        }
+    }
+    next();
+});
+
+module.exports = mongoose.model(COLLECTION_NAME, UserSchema);
