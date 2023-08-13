cdcdd4e913c908f4722b704614c6eea31e3d308b
Alain Guillon <zyrass@outlook.fr>
2023-08-13 16:40:30 +0200

    feature: Create many modules controller
    Created postModule, getAllModules and getModuleByID

--- router/api.v1.routes.js
+++ router/api.v1.routes.js
@@ -2,10 +2,18 @@ const router = require('express').Router();
 const ApiController = require('../controllers/Api.controller');
 
 router.get('/', ApiController.getHome);
+
+// Users
 router.get('/users', ApiController.getAllUsers);
 router.get('/users/:id', ApiController.getUserByID);
 router.delete('/users/:id', ApiController.deleteUserByID);
 
+// Modules
+router.post('/modules', ApiController.postModule);
+router.get('/modules', ApiController.getAllModules);
+router.get('/modules/:id', ApiController.getModuleByID);
+
+// SignUp & SignIn
 router.post('/signup', ApiController.postSignUp);
 router.post('/signin', ApiController.postSignIn);
 
