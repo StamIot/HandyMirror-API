4e1b8c5987b614b4347babf8262199527fe6c711
Alain Guillon <zyrass@outlook.fr>
2023-08-13 10:15:06 +0200

    feature: Create a deleteUserByID controller

--- router/api.v1.routes.js
+++ router/api.v1.routes.js
@@ -4,6 +4,7 @@ const ApiController = require('../controllers/Api.controller');
 router.get('/', ApiController.getHome);
 router.get('/users', ApiController.getAllUsers);
 router.get('/users/:id', ApiController.getUserByID);
+router.delete('/users/:id', ApiController.deleteUserByID);
 
 router.post('/signup', ApiController.postSignUp);
 router.post('/signin', ApiController.postSignIn);
